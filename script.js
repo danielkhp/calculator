8c6d5ab164eecf9f5dbce22c459dc33db4494b7b
Daniel <danielkhp@gmail.com>
2023-09-13 15:31:11 -0500

    Add an operator function that calls one of the math functions

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function add(a, b) {
-  return (a + b)
+let operand1
+let operand2
+let operator
+
+let operators = {
+  add:
+    function add(a, b) {
+      return (a + b)
+    },
+  subtract:
+    function subtract(a, b) {
+      return (a - b)
+    },
+  multiply:
+    function multiply(a, b) {
+      return (a * b)
+    },
+  divide:
+    function divide(a, b) {
+      return (a / b)
+    },
 }
 
-function subtract(a, b) {
-  return (a - b)
-}
-
-function multiply(a, b) {
-  return (a * b)
-}
-
-function divide(a, b) {
-  return (a / b)
+function operate(operand1, operand2, operator) {
+  return operators[operator](operand1, operand2)
 }
